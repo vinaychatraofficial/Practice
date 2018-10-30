e479f62659f339678d4730fedc2da78ba3031008
vinaychatraofficial <vinaychatraofficial@gmail.com>
2018-10-30 19:08:22 +0530

    added

---
+++ src/epi/searching/FirstOccurenceK.java
@@ -0,0 +1,34 @@
+package epi.searching;
+
+/**
+ * @author vcprabha
+ * 12.1 Search a sorted array for the first occurence of k
+ */
+public class FirstOccurenceK {
+
+	public static void main(String[] args) {
+		int[] arr = {-14,-10,2,108,108,243,285,285,285,401};
+		System.out.println(findFirstOccurence(arr, 285));
+	}
+	
+	private static Integer findFirstOccurence(int[] arr, int key){
+		int firstOccurence = -1;
+		firstOccurence = findFirstOccurence(arr,key,firstOccurence,0,arr.length-1);
+		return firstOccurence;
+	}
+
+	private static int findFirstOccurence(int[] arr, int key, int firstOccurence, int low, int high) {
+		if(high<low)
+			return firstOccurence;
+		int mid = low+(high-low)/2;
+		if(arr[mid]==key){
+			firstOccurence=mid;
+			return findFirstOccurence(arr, key, firstOccurence, low, mid-1);
+		}else if(key<arr[mid]){
+			return findFirstOccurence(arr, key, firstOccurence, low, mid-1);
+		}else{
+			return findFirstOccurence(arr, key, firstOccurence, mid+1, high);
+		}
+	}
+
+}
---
+++ src/epi/searching/SearchEntryEqualToIndex.java
@@ -0,0 +1,29 @@
+package epi.searching;
+
+/**
+ * @author vcprabha
+ * 12.2 Search a sorted array for an entry equal to index
+ */
+public class SearchEntryEqualToIndex {
+
+	public static void main(String[] args) {
+		int[] arr = {-2,0,2,3,6,7,9};
+		System.out.println(findIndex(arr));
+	}
+	
+	private static int findIndex(int[] arr){
+		int low=0, high=arr.length-1;
+		while(low<=high){
+			int mid = low+(high-low)/2;
+			if(arr[mid]==mid){
+				return arr[mid];
+			}else if(arr[mid]<mid){
+				low=mid+1;
+			}else{
+				high=mid-1;
+			}
+		}
+		return -1;
+	}
+
+}
