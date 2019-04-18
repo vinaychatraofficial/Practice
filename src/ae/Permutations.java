76fe5e61f0768682d6b9fad875662fd49ad194f9
vinaychatraofficial <vinaychatraofficial@gmail.com>
2019-04-19 00:02:43 +0530

    modified

--- src/ae/Permutations.java
+++ src/ae/Permutations.java
@@ -1,34 +1,43 @@
 package ae;
 
 import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Set;
 
 public class Permutations {
 
 	public static ArrayList<ArrayList<Integer>> getPermutations(ArrayList<Integer> array) {
 		ArrayList<ArrayList<Integer>> res = new ArrayList<>();
 		ArrayList<Integer> visited = new ArrayList<>();
-    for(Integer num:array){ 
-			visited = new ArrayList<>();
+		//faster checking for visited nodes
+		Set<Integer> set = new HashSet<>();
+		for(Integer num:array){ 
 			visited.add(num);
-			getPermutations(array, res, visited);
+			set.add(num);
+			getPermutations(array, res, set, visited);
+			visited.remove(num);
+			set.remove(num);
 		}
-		
+
 		return res;
-  }
-	
+	}
+
 	private static void getPermutations(ArrayList<Integer> array, 
-																			ArrayList<ArrayList<Integer>> res,
-																			ArrayList<Integer> visited){
+			ArrayList<ArrayList<Integer>> res,
+			Set<Integer> set,
+			ArrayList<Integer> visited){
 		if(visited.size()==array.size()){
 			res.add(new ArrayList(visited));
 			return;
 		}
-		
+
 		for(Integer num:array){
-			if(!visited.contains(num)){
+			if(!set.contains(num)){
 				visited.add(num);
-				getPermutations(array, res, visited);
+				set.add(num);
+				getPermutations(array, res, set, visited);
 				visited.remove(visited.size()-1);
+				set.remove(num);
 			}
 		}
 	}
